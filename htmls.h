49b15acb5b72d78ff96b2bd7d28e5313b4080725
romeocovaci <romeo.covaci@gmail.com>
2019-09-08 20:15:30 +0300

    update jquery to 3.41 and 1.4.5 mobile

--- htmls.h
+++ htmls.h
@@ -132,9 +132,9 @@ xhr.send(fd);
 const char sta_update_html[] PROGMEM = R"(<head>
 <title>Sprinks Firmware Update</title>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
-<link rel='stylesheet' href='http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css' type='text/css'>
-<script src='http://code.jquery.com/jquery-1.9.1.min.js' type='text/javascript'></script>
-<script src='http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js' type='text/javascript'></script>
+<link rel='stylesheet' href='http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css' type='text/css'>
+<script src='http://code.jquery.com/jquery-3.4.1.min.js' type='text/javascript'></script>
+<script src='http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js' type='text/javascript'></script>
 </head>
 <body>
 <div data-role='page' id='page_update'>
